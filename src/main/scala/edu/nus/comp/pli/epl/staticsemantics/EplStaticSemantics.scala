4707190c3b8fd67beb0e0be2cbde8fbfefbefcc0
jburkeedu <1Hillheaddrive>
2019-02-21 21:19:30 +0800

    Complete

--- src/main/scala/edu/nus/comp/pli/epl/staticsemantics/EplStaticSemantics.scala
+++ src/main/scala/edu/nus/comp/pli/epl/staticsemantics/EplStaticSemantics.scala
@@ -7,9 +7,13 @@ import edu.nus.comp.pli.epl.parser.EplAST._
   *
   */
 object EplStaticSemantics {
+
   sealed trait ResultType
+
   case object ResultTypeInt extends ResultType
+
   case object ResultTypeBool extends ResultType
+
   case object ResultTypeError extends ResultType
 
   def inferType(ast: Expression): ResultType = ast match {
@@ -28,27 +32,28 @@ object EplStaticSemantics {
       case _ => ResultTypeError
     }
     case Bin(Sub, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeInt
       case _ => ResultTypeError
     }
     case Bin(Mul, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeInt
       case _ => ResultTypeError
     }
     case Bin(Div, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeInt
       case _ => ResultTypeError
     }
     case Bin(Pow, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeInt
       case _ => ResultTypeError
     }
     case Bin(Eq, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeBool, ResultTypeBool) => ResultTypeBool
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeBool
       case _ => ResultTypeError
     }
     case Bin(NEq, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeBool, ResultTypeBool) => ResultTypeBool
       case _ => ResultTypeError
     }
     case Bin(Lt, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
@@ -56,27 +61,28 @@ object EplStaticSemantics {
       case _ => ResultTypeError
     }
     case Bin(Lte, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeBool
       case _ => ResultTypeError
     }
     case Bin(Gt, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeBool
       case _ => ResultTypeError
     }
     case Bin(Gte, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeInt, ResultTypeInt) => ResultTypeBool
       case _ => ResultTypeError
     }
     case Bin(And, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeBool, ResultTypeBool) => ResultTypeBool
       case _ => ResultTypeError
     }
     case Bin(Or, exprLeft, exprRight) => (inferType(exprLeft), inferType(exprRight)) match {
-      //add your code
+      case (ResultTypeBool, ResultTypeBool) => ResultTypeBool
       case _ => ResultTypeError
     }
     case IfElse(cond, ifbranch, elsebranck) => (inferType(cond), inferType(ifbranch), inferType(elsebranck)) match {
-      //add your code
+      case (ResultTypeBool, ResultTypeInt, ResultTypeInt) => ResultTypeInt
+      case (ResultTypeBool, ResultTypeBool, ResultTypeBool) => ResultTypeBool
       case _ => ResultTypeError
     }
   }
